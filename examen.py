c347fef37ad584e3a553dc4413c00a4fb684f759
nnatxo <nnatxo@gmail.com>
2021-03-24 11:38:56 +0100

    checkpoint 4

--- examen.py
+++ examen.py
@@ -1,18 +1,5 @@
 from autor import *
 from libro import *
-"""
-2. Crea una clase Libro en un fichero libro.py que tenga un campo autor, un campo titulo
-y un campo anyo con estos nombres. Crea tambi´en una clase Autor en un fichero autor.py
-que tenga un campo id_autor, un campo nombre y un campo apellido.
-En el main de examen.py se crear´a una lista de varios objetos Libro y se llamar´a a
-una funci´on mas_antiguos pas´andole esta lista y un a˜no. La funci´on devolver´a una lista
-de los t´ıtulos de libros (´unicamente el t´ıtulo) cuyo a˜no es igual o anterior al pasado
-por par´ametro. Si se le pasa un a˜no menor que 1900 o mayor que 2021, se emitir´a una
-excepci´on ValueError indicando que el a˜no no es v´alido.
-Se pide tambi´en hacer por lo menos 3 tests significativos para probar la funcionalidad
-de mas_antiguos
-
-"""
 
 def get_list(filename):
     word_dic = {}
@@ -37,26 +24,28 @@ def get_list(filename):
 def mas_antiguos(book_list, year):
     if year > 2021 or year < 1900:
         raise ValueError("Year is not valid.")
-    ar = [x for x in book_list if x.get_data()[2] <= year]
-    for i in ar:
-        print(i.get_data())
-    return 1
+    ar = [x.get_data()[1] for x in book_list if x.get_data()[2] <= year]
+    return ar
 def ejercicio1():
+    print("------ Ejercicio 1 -------")
     try:
         get_list("palabras.txt")
     except ValueError:
         print("File is empty.")
 
 def ejercicio2():
+    print("------ Ejercicio 2 -------")
     autor1 = Autor("AA123","Pedro","Ramirez Espinosa")
     autor2 = Autor("BA443","Julio","Mendez Ramirez")
     autor3 = Autor("AG553","Fabio","Espinola Jimenez")
+    lista_libros=[Libro(autor1,"Cómo hacerse rico con inmuebles", 1992), Libro(autor2,"Geranios y sus misterios", 2007), Libro(autor3,"El temible hombre pollo", 2021), Libro(autor1,"Cómo perdí mi casa y mi familia", 2014)]
 
-    lista_libros=[Libro(autor1,"Cómo ser rico con inmuebles", 1992), Libro(autor2,"Geranios y sus misterios", 2007), Libro(autor3,"El temible hombre pollo", 2021), Libro(autor1,"Cómo perdí mi casa y mi familia", 2014)]
     try:
-        mas_antiguos(lista_libros, 2019)
+        lista_titulos = mas_antiguos(lista_libros, 2021)
+        for i in lista_titulos:
+            print i
     except ValueError:
         print("Year is not valid.")
 
-#ejercicio1()
-#ejercicio2()
+ejercicio1()
+ejercicio2()
--- test.py
+++ test.py
@@ -7,17 +7,15 @@ class Pruebas(unittest.TestCase):
         autor1 = Autor("AA123","Pedro","Ramirez Espinosa")
         autor2 = Autor("BA443","Julio","Mendez Ramirez")
         autor3 = Autor("AG553","Fabio","Espinola Jimenez")
+        lista_libros=[Libro(autor1,"Cómo hacerse rico con inmuebles", 1992), Libro(autor2,"Geranios y sus misterios", 2007), Libro(autor3,"El temible hombre pollo", 2021), Libro(autor1,"Cómo perdí mi casa y mi familia", 2014)]
+        
+        #Testeamos los años inválidos
+        self.assertRaisesRegex(ValueError, "Year is not valid.", mas_antiguos, lista_libros, 1437)
+        self.assertRaisesRegex(ValueError, "Year is not valid.", mas_antiguos, lista_libros, 2135)
 
-        lista_libros=[Libro(autor1,"Cómo ser rico con inmuebles", 1992), Libro(autor2,"Geranios y sus misterios", 2007), Libro(autor3,"El temible hombre pollo", 2021), Libro(autor1,"Cómo perdí mi casa y mi familia", 2014)]
-        try:
-            mas_antiguos(lista_libros, 2019)
-        """
-        self.assertIsInstance(flight3, Flight)
-        self.assertIsInstance(flight3, Flight)
-        self.assertEqual(seating[18]["E"], ("James", "Ford", "56278665F"))
-        self.assertRaisesRegex(ValueError, "Seat already occupied.", f1.reallocate_passenger, "12A", "13B")
-        self.assertRaisesRegex(ValueError, "Seat already occupied.", f1.reallocate_passenger, "12A", "13B")
-        """
+        #Testeamos que funcione bien
+        lista_libros_antes_dosmil = mas_antiguos(lista_libros, 2000)
+        self.assertEqual(lista_libros_antes_dosmil[0], "Cómo hacerse rico con inmuebles")
 
 
 class Suite(unittest.TestSuite):
